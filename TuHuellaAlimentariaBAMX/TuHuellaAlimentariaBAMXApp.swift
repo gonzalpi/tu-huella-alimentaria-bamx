32ef20a73e55269cb34b5f26667844adb612e621
álex gonzalpi <alex@gonzalpi.com>
2022-10-22 14:10:22 -0500

    Initialize XCode project

---
+++ TuHuellaAlimentariaBAMX/TuHuellaAlimentariaBAMXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuHuellaAlimentariaBAMXApp.swift
+//  TuHuellaAlimentariaBAMX
+//
+//  Created by álex on 22.10.22.
+//
+
+import SwiftUI
+
+@main
+struct TuHuellaAlimentariaBAMXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TuHuellaAlimentariaBAMXUITests/TuHuellaAlimentariaBAMXUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TuHuellaAlimentariaBAMXUITestsLaunchTests.swift
+//  TuHuellaAlimentariaBAMXUITests
+//
+//  Created by álex on 22.10.22.
+//
+
+import XCTest
+
+final class TuHuellaAlimentariaBAMXUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
