22e3d79a396e00e9b6def569855da47aa3c77e40
álex gonzalpi <alex@gonzalpi.com>
2022-10-23 13:56:58 -0500

    Add Splash screen

--- TuHuellaAlimentariaBAMX/ContentView.swift
+++ TuHuellaAlimentariaBAMX/ContentView.swift
@@ -7,20 +7,39 @@
 
 import SwiftUI
 
+let redBgColor = Color(#colorLiteral(red: 219/256, green: 62/256, blue: 76/256, alpha: 1))
+let splashBgColor = Color(#colorLiteral(red: 153/256, green: 0/256, blue: 19/256, alpha: 1))
+// let textColor =
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Splash()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Splash: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                Image("logo")
+                    .resizable()
+                    .frame(width: 106, height: 133)
+                Spacer()
+                Text("Derechos Reservados 2022")
+                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.4)))
+            }
+            Spacer()
+        }
+        .background(splashBgColor)
     }
 }
