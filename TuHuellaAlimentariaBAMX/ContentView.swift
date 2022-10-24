1507d00af2d59c777a6bb71b963035ceb9cef113
álex gonzalpi <alex@gonzalpi.com>
2022-10-24 13:18:02 -0500

    Add Login screen

--- TuHuellaAlimentariaBAMX/ContentView.swift
+++ TuHuellaAlimentariaBAMX/ContentView.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 let redBgColor = Color(#colorLiteral(red: 219/256, green: 62/256, blue: 76/256, alpha: 1))
+let creamBgColor = Color(#colorLiteral(red: 242/256, green: 230/256, blue: 211/256, alpha: 1))
 let splashBgColor = Color(#colorLiteral(red: 153/256, green: 0/256, blue: 19/256, alpha: 1))
 // let textColor =
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Splash()
+            Login()
         }
     }
 }
@@ -37,9 +38,104 @@ struct Splash: View {
                 Spacer()
                 Text("Derechos Reservados 2022")
                     .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.4)))
+                    .font(.system(size: 15))
             }
             Spacer()
         }
         .background(splashBgColor)
+    }
+}
+
+struct Login: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Circle()
+                    .fill(redBgColor)
+                    .padding(.top, -450)
+                    .frame(width: 625, height: 625)
+                Spacer()
+            }
+            VStack {
+                Spacer()
+                Text("¡Te damos la\nbienvenida!")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, weight: .semibold, design: .default))
+                Image("logo")
+                    .resizable()
+                    .frame(width: 106, height: 133)
+                Spacer()
+                VStack {
+                    Button(action: {}, label: {
+                        LoginButton(txt: "Iniciar sesión con Google", img: "google", fgColor: .gray, bgColor: .white)
+                    })
+                    Button(action: {}, label: {
+                        LoginButton(txt: "Iniciar sesión con Apple", img: "apple", fgColor: .white, bgColor: .black)
+                    })
+                    Button(action: {}, label: {
+                        HStack {
+                            Text("seguir")
+                                .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.2)))
+                                .padding(.trailing, -2)
+                            Text("anónimo")
+                                .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.2)))
+                                .padding(.leading, -2)
+                                .font(.system(size: 20, weight: .bold))
+                        }
+                    })
+                    .padding(.top, 45)
+
+                }
+                Spacer()
+                HStack {
+                    Text("Continuando aceptas los")
+                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.2)))
+                        .font(.system(size: 15))
+                        .padding(.trailing, -2)
+                    Button(action: {}, label: {
+                        Text("Términos y Condiciones")
+                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.2)))
+                            .font(.system(size: 15, weight: .bold))
+                            .padding(.leading, -2)
+                    })
+                }
+            }
+        }
+        .background(creamBgColor)
+    }
+}
+
+struct LoginButton: View {
+    let txt: String,
+        img: String,
+        fgColor: Color,
+        bgColor: Color
+
+    init(txt: String,
+         img: String,
+         fgColor: Color,
+         bgColor: Color) {
+        self.txt = txt
+        self.img = img
+        self.fgColor = fgColor
+        self.bgColor = bgColor
+    }
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(bgColor)
+                .frame(width: 345, height: 54)
+                .cornerRadius(10)
+            HStack {
+                Image(img)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24, height: 24)
+                Text(txt)
+                    .foregroundColor(fgColor)
+                    .font(.system(size: 20, weight: .semibold))
+            }
+        }
     }
 }
