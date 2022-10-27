85653915f2c911e596134b5349e5e4cfae2e4f8d
álex gonzalpi <alex@gonzalpi.com>
2022-10-26 20:54:50 -0500

    Add Welcome screen

--- TuHuellaAlimentariaBAMX/ContentView.swift
+++ TuHuellaAlimentariaBAMX/ContentView.swift
@@ -15,7 +15,7 @@ let splashBgColor = Color(#colorLiteral(red: 153/256, green: 0/256, blue: 19/256
 struct ContentView: View {
     var body: some View {
         VStack {
-            Login()
+            Welcome(name: "Alejandro")
         }
     }
 }
@@ -105,6 +105,63 @@ struct Login: View {
     }
 }
 
+struct Welcome: View {
+    let hello: String
+
+    init() { self.hello = "¡Hola!" }
+    init(name: String) { self.hello = "¡Hola, " + name + "!" }
+
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                VStack {
+                    HStack {
+                        Spacer(minLength: 55)
+                        Image("logo")
+                            .resizable()
+                            .frame(width: 46, height: 54)
+                        Text(hello)
+                            .font(.system(size: 40, weight: .semibold))
+                            .foregroundColor(.white)
+                        Spacer(minLength: 15)
+                    }
+                    .padding(.top)
+                    .padding(.bottom)
+                }
+                .background(redBgColor)
+                .frame(width: .infinity, height: 86)
+                .cornerRadius(43)
+                .padding(.leading, -50)
+                Spacer()
+                Text("Conozcamos tu\nhuella alimentaria")
+                    .font(.system(size: 40, weight: .regular))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                Button(action: {}, label: {
+                    ZStack {
+                        Rectangle()
+                            .fill(redBgColor)
+                            .frame(width: 191, height: 62)
+                            .cornerRadius(43)
+                        Text("Empecemos")
+                            .font(.system(size: 20, weight: .regular))
+                            .foregroundColor(.white)
+                    }
+                })
+                Spacer()
+                Button(action: {}) {
+                    FooterButton()
+                        .padding(.bottom, -80)
+                }
+            }
+            Spacer()
+        }
+        .background(creamBgColor)
+    }
+}
+
 struct LoginButton: View {
     let txt: String,
         img: String,
@@ -136,6 +193,23 @@ struct LoginButton: View {
                     .foregroundColor(fgColor)
                     .font(.system(size: 20, weight: .semibold))
             }
+        }
+    }
+}
+
+struct FooterButton: View {
+    var body: some View {
+        ZStack {
+            Image("fruits")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.size.width, height: 180)
+                .cornerRadius(43)
+                .opacity(0.3)
+            Text("¡Terminemos el\nhambre!\n\n")
+                .font(.system(size: 32, weight: .regular))
+                .multilineTextAlignment(.center)
+                .foregroundColor(.white)
         }
     }
 }
