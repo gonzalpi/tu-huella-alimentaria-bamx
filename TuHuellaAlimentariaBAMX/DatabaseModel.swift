c63a209250bbc92bf09aa3b643e357ed19939b33
han rodríguez <han@gonzalpi.com>
2022-11-29 17:57:35 -0600

    Log data post time

--- TuHuellaAlimentariaBAMX/DatabaseModel.swift
+++ TuHuellaAlimentariaBAMX/DatabaseModel.swift
@@ -11,19 +11,25 @@ import Firebase
 class DatabaseModel: ObservableObject {
     var ref: DatabaseReference! = Database.database().reference()
     func setUserData(email: String, name: String, footprint: Int, donation: Int) {
+        let startTime = Date.now
         let newEmail = email.replacingOccurrences(of: "[\\.\\$\\[\\]\\#]", with: ",", options: .regularExpression)
         self.ref.child("results").child(newEmail).setValue([
             "name": name,
             "footprint": footprint,
             "latest_donation": donation,
             "date": Date.now.formatted(date: .long, time: .shortened)
-        ])
+        ]) { _, _ in
+            print("Send user data duration:", String(Date.now.timeIntervalSince(startTime)))
+        }
     }
     func setAnonymousData(footprint: Int, donation: Int) {
+        let startTime = Date.now
         self.ref.child("results").child(String(NSDate().timeIntervalSince1970).replacingOccurrences(of: ".", with: ",")).setValue([
             "footprint": footprint,
             "latest_donation": donation,
             "date": Date.now.formatted(date: .long, time: .shortened)
-        ])
+        ]) { _, _ in
+            print("Send anonymous data duration:", String(Date.now.timeIntervalSince(startTime)))
+        }
     }
 }
